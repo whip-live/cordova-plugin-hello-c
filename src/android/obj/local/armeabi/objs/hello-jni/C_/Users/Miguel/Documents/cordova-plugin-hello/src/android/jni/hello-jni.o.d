38e3592fbaf9b03364ed073ac1c3dbef02781190
domq <dominique@quatravaux.org>
2016-09-23 19:24:45 +0200

    Delete hello-jni.o.d
    This is just bad.

--- src/android/obj/local/armeabi/objs/hello-jni/C_/Users/Miguel/Documents/cordova-plugin-hello/src/android/jni/hello-jni.o.d
+++
@@ -1,2 +0,0 @@
-C:/Users/Miguel/Documents/cordova-plugin-hello/src/android/obj/local/armeabi/objs/hello-jni/C_\Users\Miguel\Documents\cordova-plugin-hello\src\android\jni\hello-jni.o: \
- C:\Users\Miguel\Documents\cordova-plugin-hello\src\android\jni\hello-jni.c
