92b33582913b4f6b7fca5190eafe7653c0409192
Dave Alden <dpa99c@gmail.com>
2017-04-08 09:22:03 +0100

    Add shared library components

---
+++ src/common/mylib/mylib.c
@@ -0,0 +1,16 @@
+//
+//  mylib.c
+//  Cross-platform C library
+//
+
+#include "mylib.h"
+#include "mycomponent.h"
+#include <stdlib.h>
+
+int main(){
+    return 0;
+}
+
+int calculate(int y, int z) {
+    return sum(y,z)*z;
+}
