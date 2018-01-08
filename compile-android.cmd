7414034015295f3e2ddd701eb104ba3efb4a89a5
Dave Alden <dpa99c@gmail.com>
2018-01-08 19:48:44 +0000

    Combine Android.mk to single OS-conditional version

--- compile-android.cmd
+++ compile-android.cmd
@@ -1,4 +1,3 @@
 @echo off
 cd src\android
-cp jni\Android.mk.windows jni\Android.mk
 ndk-build.cmd
--- src/android/jni/Android.mk
+++ src/android/jni/Android.mk
@@ -1,25 +1,36 @@
-# Android Makefile for OSX/Linux
+# Android Makefile
+
+ifeq ($(OS),Windows_NT)
+	PATH_SEP := \ 
+else
+	PATH_SEP := /
+endif
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #traverse all the directory and subdirectory
 define walk
-  $(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call walk, $(e)))
+  $(wildcard $(1)) $(foreach e, $(wildcard $(1)$(PATH_SEP)*), $(call walk, $(e)))
 endef
 
 LOCAL_MODULE := helloc
 
-INCLUDE_LIST := ${shell find $(LOCAL_PATH)/../../common/ -type d}
+ifeq ($(OS),Windows_NT)
+	INCLUDE_LIST := ${shell dir $(LOCAL_PATH)\..\..\common\ /ad /b /s}
+else
+	INCLUDE_LIST := ${shell find $(LOCAL_PATH)/../../common/ -type d}
+endif
 
-SRC_LIST := $(wildcard $(LOCAL_PATH)/*.c)
-SRC_LIST += $(filter %.c, $(call walk, $(LOCAL_PATH)/../../common))
+
+SRC_LIST := $(wildcard $(LOCAL_PATH)\*.c)
+SRC_LIST += $(filter %.c, $(call walk, $(LOCAL_PATH)$(PATH_SEP)..$(PATH_SEP)..$(PATH_SEP)common))
 
 
 #$(warning SRC_LIST:$(SRC_LIST))
 #$(warning INCLUDE_LIST:$(INCLUDE_LIST))
 
 LOCAL_C_INCLUDES := $(INCLUDE_LIST)
-LOCAL_SRC_FILES := $(SRC_LIST:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES := $(SRC_LIST:$(LOCAL_PATH)$(PATH_SEP)%=%)
 
 include $(BUILD_SHARED_LIBRARY)
